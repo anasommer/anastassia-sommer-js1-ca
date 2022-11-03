fd59b01bc41e125e0e836cf1c025fd3a21568549
anasommer <68922341+anasommer@users.noreply.github.com>
2022-11-03 20:09:26 +0100

    initial commit

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const apiKey = `c0bb74b272e24186bad796b6d0217d57`;
+const country = `no`;
+const url = `https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${apiKey}`;
+
+console.log(url);
