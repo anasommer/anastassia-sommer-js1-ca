3f21db97d1ec6cc7e48814e1ecb1ff55e614f3f5
anasommer <68922341+anasommer@users.noreply.github.com>
2022-11-03 20:21:55 +0100

    api request

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
+// NYT Best Sellers books API url with a key
 const apiKey = `yjKq4yraOdoP9mrRWUQAko5lIqBiDeX3`;
 const url = `https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=${apiKey}`;
 
-console.log(url);
+async function getData() {
+  try {
+    const response = await fetch(url);
+    const result = await response.json();
+    const books = result.results.books;
+    console.log(books);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getData();
