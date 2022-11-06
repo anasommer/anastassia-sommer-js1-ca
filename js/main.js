0015c13261bf3dd19fb7ea1dbd10f6e2645d70dd
anasommer <anastassia.sommer@gmail.com>
2022-11-06 16:26:32 +0100

    add error message-styling

--- js/main.js
+++ js/main.js
@@ -1,14 +1,19 @@
 // NYT Best Sellers books API url with a key
 const apiKey = `yjKq4yraOdoP9mrRWUQAko5lIqBiDeX3`;
-const url = `https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=${apiKey}`;
+const url = `https://api.nytimes.com/sv/books/v3/lists/current/hardcover-fiction.json?api-key=${apiKey}`;
+
+const container = document.querySelector(".container");
 
 async function getData() {
   try {
     const response = await fetch(url);
     const result = await response.json();
     const books = result.results.books;
-    console.log(books);
+    container.classList.remove("lds-default");
   } catch (error) {
+    container.classList.remove("lds-default");
+    container.classList.add("error");
+    container.innerHTML = `<div>Sorry, something went wrong. Try to reload a page.</div>`;
     console.log(error);
   }
 }
