263e2926fe4c7dd9b905b7ef9ad92bad0412f236
anasommer <anastassia.sommer@gmail.com>
2022-11-07 18:11:57 +0100

    add success message

--- js/contact.js
+++ js/contact.js
@@ -7,6 +7,7 @@ const email = document.querySelector("#email");
 const emailError = document.querySelector("#email-error");
 const address = document.querySelector("#address");
 const addressError = document.querySelector("#address-error");
+const successMsg = document.querySelector("#success");
 
 function checkLength(value, minLength) {
   if (value.trim().length > minLength) {
@@ -23,6 +24,8 @@ function checkEmail(email) {
 }
 
 function validateForm(event) {
+  const subjectMinLength = 10;
+  const addressMinLength = 25;
   event.preventDefault();
 
   if (checkLength(name.value, 0) === true) {
@@ -31,22 +34,31 @@ function validateForm(event) {
     nameError.style.display = "block";
   }
 
-  if (checkLength(subject.value, 10) === true) {
+  if (checkLength(subject.value, subjectMinLength) === true) {
     subjectError.style.display = "none";
   } else {
     subjectError.style.display = "block";
   }
 
-  if (checkEmail(email.value) === true) {
+  if (checkEmail(email.value)) {
     emailError.style.display = "none";
   } else {
     emailError.style.display = "block";
   }
 
-  if (checkLength(address.value, 25) === true) {
+  if (checkLength(address.value, addressMinLength) === true) {
     addressError.style.display = "none";
   } else {
     addressError.style.display = "block";
+  }
+
+  if (
+    name.value &&
+    subject.value.length > subjectMinLength &&
+    email.value &&
+    address.value.length > addressMinLength
+  ) {
+    successMsg.innerHTML = `<h2 style="color:red; background-color:#E8E9EB ">Thank you!</h2>`;
   }
 }
 
